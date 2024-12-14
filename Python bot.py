e788b7a645855b80f569e1099f38f5fb05b7355c
monetagads <monetag@mail.com>
2024-12-14 15:17:34 +0600

    Update Python bot.py

--- Python bot.py
+++ Python bot.py
@@ -1,21 +1,20 @@
-from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-# Command to provide Mini App link
-def start(update: Update, context: CallbackContext):
+def start(update, context):
     keyboard = [
-        [InlineKeyboardButton("Open Mini App", web_app={"url": "https://monetagads.github.io/ads/"})]
+        [InlineKeyboardButton("ADS 1", url="https://luglawhaulsano.net/4/8639415")],
+        [InlineKeyboardButton("ADS 2", url="https://luglawhaulsano.net/4/8639413")],
+        [InlineKeyboardButton("ADS 3", url="https://luglawhaulsano.net/4/8639353")],
+        [InlineKeyboardButton("ADS 4", url="https://luglawhaulsano.net/4/8639412")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Click below to open the Mini App:", reply_markup=reply_markup)
+    update.message.reply_text("Choose an option:", reply_markup=reply_markup)
 
 def main():
-    # Replace 'YOUR_TOKEN' with your BotFather token
-    updater = Updater("7734607736:AAEBmRtMbDusnecfgVB7IvXvIpr524wSOE4", use_context=True)
+    updater = Updater("7583196010:AAGXKsZt55IGeVddXJPMBFwxj---NJDt2fQ", use_context=True)
     dp = updater.dispatcher
-
     dp.add_handler(CommandHandler("start", start))
-
     updater.start_polling()
     updater.idle()
 
